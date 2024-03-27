cf15425161998385b131ca522dbced691495a580
Eialist <elsinoree@gmail.com>
2024-03-27 16:00:32 +0100

    jwoiqd

--- server/index.js
+++ server/index.js
@@ -60,7 +60,7 @@ app.get("/", async (req, res) => {
     return res.json(item)
 })
 
-fetchWords().then(err => {
+fetchCollection(year4).then(err => {
     if (err) { console.error(err); return false; }
     // connection to mongo is successful, listen for requests
     app.listen(port, () => {
